335086d3d4808f853069f0777cdc4aa7f65142a4
Yov54 <yovi.revikasari03@gmail.com>
2026-05-13 14:22:30 +0700

    Update base path to /pixel-logic/ for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/pixel-logic-dev/',
+  base: '/pixel-logic/',
   plugins: [
     react(),
     tailwindcss(),
